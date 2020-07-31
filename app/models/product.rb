37339f41be778c6eb136e8369b895507d16f0416
higasimaru <andyourbirdcansing@icloud.com>
2020-07-31 20:21:39 +0900

    Add association and validation and define products index

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  has_many :images
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :price, presence: true, length: { maximum: 11 }
+  default_scope -> { order(created_at: :desc) }
 end
