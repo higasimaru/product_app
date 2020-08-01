b830c8af0c52feb8d86d3bde76f0dbe890534352
higasimaru <andyourbirdcansing@icloud.com>
2020-08-01 15:50:32 +0900

    Add products create and association for images and modify form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,27 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.all.includes(:image)
+    @products = Product.all.includes(:images)
   end
 
   def new
     @product = Product.new
     @product.images.build
   end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :price, images_attributes: :url)
+    end
 
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,5 @@ class Product < ApplicationRecord
   validates :name, presence: true, length: { maximum: 40 }
   validates :price, presence: true, length: { maximum: 11 }
   default_scope -> { order(created_at: :desc) }
+  accepts_nested_attributes_for :images
 end
