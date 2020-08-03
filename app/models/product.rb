d83d34bc15e0f6ab343da4f2d5c76adcd66c7ea2
higasimaru <andyourbirdcansing@icloud.com>
2020-08-03 22:15:24 +0900

    Modify products new page

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   has_many :images
   validates :name, presence: true, length: { maximum: 40 }
   validates :price, presence: true, numericality: { only_integer: true,
-    greater_than: 299, less_than: 100000000 }
+    greater_than: 299, less_than: 10000000 }
   default_scope -> { order(created_at: :desc) }
   accepts_nested_attributes_for :images
 end
