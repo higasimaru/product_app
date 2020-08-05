c60ef8dd8b572b77b60c8637824e594bf01baf18
higasimaru <andyourbirdcansing@icloud.com>
2020-08-05 13:54:13 +0900

    Add error messages helper

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -4,4 +4,12 @@ module ProductsHelper
     "¥#{price.to_s(:delimited)}"
   end
 
+  def display_error_messages(object, attribute)
+    content_tag(:div, class: "error-messages") do
+      object.errors.full_messages_for(attribute).map do |message|
+      concat(content_tag(:p, message, class: "error-message"))
+      end
+    end  
+  end
+
 end
