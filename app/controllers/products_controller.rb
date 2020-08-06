e996665dee5abe9814652d7a75c5ab94319edcaf
higasimaru <andyourbirdcansing@icloud.com>
2020-08-06 16:58:35 +0900

    Add style for flash and modify validation for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,8 +11,8 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    @product.url = params[:product][:images_attributes]   
-    @product.images.new if @product.url.nil?
+    @product.url = url_params 
+    @product.images.new if @product.url.blank?
     if @product.save
       flash[:success] = "出品しました。"
       redirect_to root_url
@@ -25,6 +25,10 @@ class ProductsController < ApplicationController
   private
     def product_params
       params.require(:product).permit(:name, :price, images_attributes: [:url])
+    end
+
+    def url_params
+      params.require(:product).permit(images_attributes: :url)
     end
 
 
