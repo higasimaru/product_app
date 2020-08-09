a86232cdbeffaf4323fcb6873c2e6dbcc04a2fbb
higasimaru <andyourbirdcansing@icloud.com>
2020-08-09 21:27:27 +0900

    Modify images preview

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -9,6 +9,10 @@ $(document).on('turbolinks:load', () => {
     return html;
   }
 
+  const buildPreviewFileField = (targetIndex) => {
+    $(`div[data-index = "${targetIndex}"]`).appendTo('div[data-preview="' + targetIndex + '"]')
+  }
+
   const buildFileField = (fileIndex) => {
     let html = `<div class="file-form" data-index="${fileIndex}" id="new-file">
                   <input type="file" name="product[images_attributes][${fileIndex}][url]" id="file">
@@ -16,31 +20,32 @@ $(document).on('turbolinks:load', () => {
     return html;
   }
 
+  let $fileLabelCopy = $('#file-label').clone(true)
   let fileIndex = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   $('.product-form').on('change', '#file', function(e) {
     const targetIndex = $(this).parent().data('index');
     const file = e.target.files[0];
     const blobUrl = window.URL.createObjectURL(file);
-    if ($('.preview-files .preview-file').length === 4) {
-      $('#file-label').appendTo('.new-file-box')
+    let $firstFileLength = $('.preview-files .preview-file').length
+    let $fileLabel = $('#file-label')
+    if ($firstFileLength === 4) {
+      $fileLabel.appendTo('.new-file-box')
     } else if ($('.preview-file').length === 9) {
-      $('#file-label').remove();
+      $fileLabel.remove();
     }
-    if ($('.preview-files .preview-file').length < 5) {
+    if ( $firstFileLength < 5) {
       $('.preview-files').append(buildPreview(targetIndex, blobUrl));
-      $(`div[data-index="${targetIndex}"]`).appendTo('div[data-preview="' + targetIndex + '"]');
-    } else if ($('.preview-files .preview-file').length >= 5) {
+      buildPreviewFileField(targetIndex);
+    } else if ($firstFileLength >= 5) {
       $('.new-preview-files').append(buildPreview(targetIndex, blobUrl));
-      $('div[data-index="' + targetIndex + '"]').appendTo(`div[data-preview="${targetIndex}"]`);      
+      buildPreviewFileField(targetIndex);     
     } 
-    $('#file-label').prepend(buildFileField(fileIndex[0]));
+    $fileLabel.prepend(buildFileField(fileIndex[0]));
     $('#file-message').hide();
     fileIndex.shift();
     fileIndex.push(fileIndex.slice(-1)[0] + 1); 
   });
-  $fileLabel = $('#file-label').clone(true)
-  console.log($fileLabel)
   $(document).on('click', '.remove-btn', function() {
     $(this).parent().remove();
     if ($('.preview-files .preview-file').length === 4) {
@@ -51,11 +56,8 @@ $(document).on('turbolinks:load', () => {
       }  
     }
     if ($('.preview-file').length === 9) {
-      console.log($('.preview-file').length)
-      $('.new-file-box').append($fileLabel)
+      $('.new-file-box').append($fileLabelCopy)
       $('#file-message').hide();
-    }
-    
+    }    
   });
-
 });
